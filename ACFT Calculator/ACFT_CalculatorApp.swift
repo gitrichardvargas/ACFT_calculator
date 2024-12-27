70c7735261fc432b9654d2593f74fe87dac3dc39
Richard Vargas <emailrichardvargas@gmail.com>
2024-12-27 14:04:15 -0600

    Initial Commit

---
+++ ACFT Calculator/ACFT_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACFT_CalculatorApp.swift
+//  ACFT Calculator
+//
+//  Created by Richard Vargas on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ACFT_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ACFT CalculatorTests/ACFT_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ACFT_CalculatorTests.swift
+//  ACFT CalculatorTests
+//
+//  Created by Richard Vargas on 12/27/24.
+//
+
+import Testing
+@testable import ACFT_Calculator
+
+struct ACFT_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
